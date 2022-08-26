ead11a545b7c31287f441ee35a2fd26d3fbefa50
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-08-26 14:56:54 -0400

    feat: instala firebase y eleimina archivos innecesarios

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_FB_API_KEY: string;
+	readonly VITE_DB_AUTH_DOMAIN: string;
+	readonly VITE_FB_PROJECT_ID: string;
+	readonly VITE_FB_STORAGE_BUCKET: string;
+	readonly VITE_FB_MESSAGINGSENDER_ID: string;
+	readonly VITE_FB_APP_ID: string;
+	readonly VITE_SENTRY_DSN: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
