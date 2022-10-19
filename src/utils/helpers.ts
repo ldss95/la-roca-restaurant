af5875871c2ca9a04c339ca0005921a61146fca0
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-18 23:33:26 -0400

    feat: crea helpers para abrir modales y evitar caracteres no numericos

---
+++ src/utils/helpers.ts
@@ -0,0 +1,21 @@
+import { Subject } from 'rxjs';
+
+/**
+ * Evita que un input pueda recibir caracteres no numericos
+ * Esta funcion puede ser insertada en un Event Listener de tipo keydown
+ */
+export function avoidNotNumerics(event: any) {
+	if(['Backspace', 'Delete', 'Tab'].includes(event.key)) {
+		return;
+	}
+
+	const isNumber = !isNaN(Number(event.key));
+	const hasPoint = event.target.value.includes('.');
+	const isFirstPoint = (event.key === '.' && !event.target.value.includes('.'));
+	const have2Decimals = hasPoint && event.target.value.split('.').pop().length === 2;
+
+	if((!isNumber && !isFirstPoint) || have2Decimals)
+		event.preventDefault();
+}
+
+export const ModalOpener$ = new Subject<'CATEGORY'>();
