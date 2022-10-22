94654e16821dbfc103f84dc179d52e151f5c8efd
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-22 17:22:22 -0400

    feat: crea diccionario local

---
+++ src/dictionary.ts
@@ -0,0 +1,18 @@
+export default {
+	es: {
+		title: ['El sabor de la', 'cocina dominicana'],
+		navbar: {
+			menu: 'Menú',
+			aboutUs: 'Nosotros',
+			contact: 'Contacto'
+		}
+	},
+	en: {
+		title: ['The taste of ', 'Dominican cuisine'],
+		navbar: {
+			menu: 'Menu',
+			aboutUs: 'About Us',
+			contact: 'Contact'
+		}
+	}
+}
