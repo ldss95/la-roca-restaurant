d0992669cfc95b1823818cda56bd12748b457e1c
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-11-01 00:09:37 -0400

    feat: inicializa firebase analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore} from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FB_API_KEY,
@@ -17,3 +18,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// Inicializa analiticas
+getAnalytics(app);
