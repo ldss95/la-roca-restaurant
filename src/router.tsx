57417e51171f8690c9db0df795c35d3ba75439bd
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-11-06 14:13:54 -0400

    feat: envia todas las rutas no existentes al landing

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import moment from 'moment';
 
 import NavBarState from '@/context/navbar/state';
@@ -6,51 +6,48 @@ import AdminNavBar from '@/layout/AdminNavBar';
 
 import Landing from '@/views/Landing';
 import Login from '@/views/Login';
-import NotFound from '@/views/NotFound';
 import Users from '@/views/Users';
 import Products from '@/views/Products';
 import Categories from '@/views/Categories';
 import Images from '@/views/Images';
 import Texts from '@/views/Texts';
 
+
+
 const Router = () => {
 	const location = useLocation();
 	const path = location.pathname;
 
-	if (['/', '/login', '/404'].includes(path)) {
+	if (['/users', '/products', '/categories', '/images', '/texts'].includes(path)) {
 		return (
-			<Routes>
-				<Route
-					path='/'
-					element={<Landing />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/404' element={<NotFound />} />
-			</Routes>
+			<NavBarState>
+				<div style={{ minHeight: '100vh', background: '#EEE1D4' }}>
+					<div className='site-layout'>
+						<div style={{ padding: 30 }}>
+							<Routes>
+								<Route path='/users' element={<Users />} />
+								<Route path='/products' element={<Products />} />
+								<Route path='/categories' element={<Categories />} />
+								<Route path='/images' element={<Images />} />
+								<Route path='/texts' element={<Texts />} />
+							</Routes>
+						</div>
+						<div style={{ textAlign: 'center' }}>
+							La Roca Restaurant ©{moment().format('YYYY')} All rights reserved
+						</div>
+					</div>
+
+					<AdminNavBar />
+				</div>
+			</NavBarState>
 		);
 	}
 
 	return (
-		<NavBarState>
-			<div style={{ minHeight: '100vh', background: '#EEE1D4' }}>
-				<div className='site-layout'>
-					<div style={{ padding: 30 }}>
-						<Routes>
-							<Route path='/users' element={<Users />} />
-							<Route path='/products' element={<Products />} />
-							<Route path='/categories' element={<Categories />} />
-							<Route path='/images' element={<Images />} />
-							<Route path='/texts' element={<Texts />} />
-							<Route path='/*' element={<Navigate to='/404' />} />
-						</Routes>
-					</div>
-					<div style={{ textAlign: 'center' }}>
-						La Roca Restaurant ©{moment().format('YYYY')} All rights reserved
-					</div>
-				</div>
-
-				<AdminNavBar />
-			</div>
-		</NavBarState>
+		<Routes>
+			<Route path='/login' element={<Login />} />
+			<Route path='/*' element={<Landing />} />
+		</Routes>
 	);
 };
 
