78ee1ea916006195cfc57fee796e2be8aed0a75a
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2024-06-25 00:33:43 -0400

    feat: elimina tiempo de carga por carga del diccionario

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -5,39 +5,39 @@ import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '@/firebase';
 
 export const useCopy = (): [CopyProps, boolean] => {
-	const [loading, setLoading] = useState(true);
+	const loading = false;
 	const [copy, setCopy] = useState<CopyProps>({
 		en: {
-			title: ['', ''],
+			title: ['The taste of ', 'Dominican cuisine'],
 			about_us: {
-				title: ['', ''],
-				subtitle: ''
+				title: ['We offer you a ', 'family atmosphere'],
+				subtitle: 'To enjoy your favorite dishes and the rich flavor of our cuisine'
 			},
 			contact_us: {
-				title: ['', ''],
-				address: ''
-				,phone: ''
+				title: ['Come and enjoy ', 'our seasoning'],
+				address: '1150 Elmwood Ave',
+				phone: '(401) 941-8090'
 			},
 			menu: {
-				title: ['', '']
+				title: ['A great variety', 'of Dominican dishes']
 			},
-			fdaWarning: '',
+			fdaWarning: 'If you have a food allergy, please speak to any member of our staff. The FDA warns that consuming raw or undercooked meats, poultry, shellfish, or eggs increases the risk of foodborne illness.',
 		},
 		es: {
-			title: ['', ''],
+			title: ['El sabor de la', 'cocina dominicana'],
 			about_us: {
-				title: ['', ''],
-				subtitle: ''
+				title: ['Te ofrecemos un', 'ambiente familiar'],
+				subtitle: 'Para disfrutar de tus platos favoritos y el sabroso sazón de nuestra cocina'
 			},
 			contact_us: {
-				title: ['', ''],
-				address: '',
-				phone: ''
+				title: ['Ven a disfrutar de ', 'nuestro sazón.'],
+				address: '1150 Elmwood Ave',
+				phone: '(401) 941-8090'
 			},
 			menu: {
-				title: ['', '']
+				title: ['Una gran variedad', 'de platos dominicanos']
 			},
-			fdaWarning: '',
+			fdaWarning: 'Si tiene una alergia alimentaria, por favor hable con cualquier miembro de nuestro personal. La FDA advierte que consumir carnes crudas o poco cocidas, aves, mariscos o huevos aumenta el riesgo de enfermedades transmitidas por los alimentos.',
 		}
 	});
 
@@ -45,17 +45,15 @@ export const useCopy = (): [CopyProps, boolean] => {
 		const ref = collection(db, 'copy');
 		const unsubscribe = onSnapshot(ref, (snap) => {
 			if (snap.empty) {
-				return setLoading(false);
+				return;
 			}
 
-			setLoading(true);
 			const copy = {} as CopyProps;
 			for (const item of snap.docs) {
 				const key = item.id as 'en' | 'es';
 				copy[key] = item.data() as LangProps;
 			}
 			setCopy(copy);
-			setLoading(false);
 		});
 
 
